f5ffbbe26e0dad13c9a929b3788894f0da311760
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-10-12 20:26:52 +0600

    everythone done, final polishing going on

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,19 +3,23 @@ import Listcard from "../Listcard/Listcard";
 import "./List.css";
 
 const List = (props) => {
-  // const { name } = props.shortlist;
+  let total = 0;
+  for (let candidate of props.shortlist) {
+    total += candidate.balance;
+  }
+  console.log(total);
   return (
     <div className="shortlist-container">
-      <h2>
-        Total shortlisted candidates:{" "}
+      <h4>
+        Shortlisted Candidates:{" "}
         <span id="shortlist-total">{props.shortlist.length}</span>
-      </h2>
+      </h4>
+      <h4> Total Monthly Salary: {total} BDT</h4>
       <div className="shortlisted-candidates">
         {props.shortlist.map((candidate) => (
           <Listcard key={candidate._id} candidate={candidate}></Listcard>
         ))}
       </div>
-      {/* <Listcard></Listcard> */}
     </div>
   );
 };
